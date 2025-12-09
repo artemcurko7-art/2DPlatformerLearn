19455265905e75bc878a01f91fd6ed0a75167453
artemcurko7-art <artemcurko7@gmail.com>
2025-12-09 17:55:07 +0300

    Update UnitRotation.cs

--- Assets/Scripts/UnitRotation.cs
+++ Assets/Scripts/UnitRotation.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class UnitRotation : MonoBehaviour
 {
-    private readonly Quaternion RotationLookRight = Quaternion.identity;
-    private readonly Quaternion RotationLookLeft = Quaternion.Euler(0, 180, 0);
+    private readonly Quaternion _rotationLookRight = Quaternion.identity;
+    private readonly Quaternion _rotationLookLeft = Quaternion.Euler(0, 180, 0);
     private readonly int _numberHorizontalX = 1;
 
     public Quaternion GetRotation(float direction) =>
-        direction == _numberHorizontalX ? RotationLookRight : RotationLookLeft;
+        direction == _numberHorizontalX ? _rotationLookRight : _rotationLookLeft;
 }
